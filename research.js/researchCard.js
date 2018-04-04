4fcfd0b4e5940d362aa6608ca6a3b400096b0285
GIP2000 <gpress2222@gmail.com>
2018-04-04 10:21:46 -0400

    research redirect added
    Added a redirect when Wikepedia articles are slightly different
    
    TODO: sievie through the information and find relevant infomration.

--- research.js/researchCard.js
+++ research.js/researchCard.js
@@ -1,18 +1,49 @@
 function getResarch()
 {
-	getStuff(); 
+	information("New York City"); 
+	information("Ice cream"); 
+	information("Hot dogs"); 
 };
 
-function getStuff()
+var information = function(search)
 {
-	var zero = []; 
+	var title = ""; 
+	var stuff = ""; 
 	$.ajax({headers: {
 				'Content-Type': "application/json",
 				'Access-Control-Allow-Origin' : "*"
 			},
 			method: "GET",
 			dataType: "jsonp",
-            url: "https://en.wikipedia.org/w/api.php?action=query&titles=New+York+City&prop=revisions&rvprop=content&format=json&formatversion=0"}).done(function(result) {
-				console.log(result.query.pages[645042].revisions[0]["*"]); 
-    });
+            url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+ encodeURIComponent(search)}).done(function(result) {
+				//console.log(result); 
+				title = result[0]; 
+				
+				$.ajax({headers: {
+				'Content-Type': "application/json",
+				'Access-Control-Allow-Origin' : "*"
+			},
+			method: "GET",
+			dataType: "jsonp",
+            url: 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles='+title}).done(function(result) {
+				//console.log(Object.keys(result.query.pages)[0]); 
+				stuff = result.query.pages[Object.keys(result.query.pages)[0]].revisions["0"]["*"]; 
+				//console.log(stuff.substr(0,10) == "#REDIRECT "); 
+				
+					if(stuff.substr(0,10) == "#REDIRECT ")
+						{
+							console.log("ran"); 
+							let regex = /\[\[(.+)\]\]/; 
+							information(stuff.match(regex)[1]); 
+
+							}
+					console.log(stuff); 
+					return stuff; 
+					
+				
+			});
+		});
+	
+
+	
 }
